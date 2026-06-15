5b3dd7b928bcdc866c397fe2af6540ec5506974a
Alfazu4167 <alfazuddin4167@gmail.com>
2026-06-15 22:15:27 +0600

    booking delete method implemented

--- index.js
+++ index.js
@@ -56,6 +56,11 @@ async function run() {
       )
       res.send(result)
     })
+    app.delete('/bookings/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await bookingCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result)
+    })
     app.post('/bookings', async (req, res) => {
       const booking = req.body
       const result = await bookingCollection.insertOne(booking);
